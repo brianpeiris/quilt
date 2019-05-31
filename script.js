0adbf4b4dc156575456b631b16ef3a16bdf8cac3
Glitch (thinkable-speedboat) <none>
2019-05-31 19:25:56 +0000

    🌘👲 Checkpoint ./script.js:535365/627 ./index.html:535365/106

--- script.js
+++ script.js
@@ -5,19 +5,27 @@ const scene = new THREE.Scene();
 const light = new THREE.DirectionalLight();
 light.position.set(0, 1, 1);
 scene.add(light);
-scene.add(new THREE.AmbientLight(0x))
+scene.add(new THREE.AmbientLight(0x888888))
 const camera = new THREE.PerspectiveCamera();
 camera.position.z = 3;
 camera.aspect = renderer.domElement.width / renderer.domElement.height;
 camera.updateProjectionMatrix();
 const controls = new THREE.OrbitControls(camera);
 controls.enablePan = false;
+controls.target.set(0, 0.25, 0)
+controls.update();
 renderer.setAnimationLoop(animate)
 function animate(){
   renderer.render(scene, camera);
 }
+const ctx = window.uv.getContext("2d");
 window.glbfile.onchange = () => {
   new THREE.GLTFLoader().load(URL.createObjectURL(window.glbfile.files[0]), gltf => {
     scene.add(gltf.scene);
+    const uvs = gltf.scene.getObjectByProperty("type", "SkinnedMesh").geometry.attributes.uv.array;
+    for (let i = 0; i < uvs.length / 2; i+=2){
+      ctx.lineTo(uvs[i] * window.uv.width, uvs[i + 1] * window.uv.height);
+    }
+    ctx.stroke();
   })
 };
