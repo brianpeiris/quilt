6d87a797e7bcad3b1a831f1b6235bf91908db84d
Glitch (hello-webpage) <none>
2019-05-31 19:12:14 +0000

    🎥👷 Checkpoint ./index.html:535365/346 ./script.js:535365/730

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 const renderer = new THREE.WebGLRenderer();
 document.body.append(renderer.domElement);
 const scene = new THREE.Scene();
+const light = new THREE.DirectionalLight();
+light.position.set(0, 1, 1);
+scene.add(light);
 const camera = new THREE.PerspectiveCamera();
 camera.position.z = 3;
 camera.aspect = renderer.domElement.width / renderer.domElement.height;
 camera.updateProjectionMatrix();
-scene.add(new THREE.Mesh(new THREE.BoxBufferGeometry()))
+const controls = new THREE.OrbitControls(camera);
+controls.enablePan = false;
 renderer.setAnimationLoop(animate)
 function animate(){
   renderer.render(scene, camera);
 }
-glbfile.onchange = () => {
-  new THREE.GltfLoader
+window.glbfile.onchange = () => {
+  new THREE.GLTFLoader().load(URL.createObjectURL(window.glbfile.files[0]), gltf => {
+    scene.add(gltf.scene);
+  })
 };
