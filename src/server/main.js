1873b5b7b60bb930b3b59b20a6729266c6e3e2e1
Brian Peiris <brianpeiris@gmail.com>
2019-06-18 07:19:46 -0700

    fix deploy

--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const ogs = require("open-graph-scraper");
-const webpack = require("webpack");
 
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
+  const webpack = require("webpack");
   const compiler = webpack({ ...require("../../webpack.config.js"), mode: "development" });
   const middleware = require("webpack-dev-middleware");
   app.use(middleware(compiler));
